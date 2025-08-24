ba87e9aed14290194f629040599cf0d40ac57e5f
Moses tembula <jameswangabivan@gmail.com>
2025-08-24 16:05:04 +0300

    fix gtag lib

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,5 +1,20 @@
+// lib/gtag.js
+
+// Your GA tracking ID
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID
+
+// Log the pageview with their URL
 export const pageview = (url) => {
-    window.gtag('config', process.env.NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID, {
+    window.gtag('config', GA_TRACKING_ID, {
         page_path: url,
-    });
-};
+    })
+}
+
+// Log specific events
+export const event = ({ action, category, label, value }) => {
+    window.gtag('event', action, {
+        event_category: category,
+        event_label: label,
+        value: value,
+    })
+}
--- pages/_app.js
+++ pages/_app.js
@@ -8,102 +8,89 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { ToastContainer } from 'react-toastify'
 import Script from 'next/script'
-import TawkTo from 'next-tawkto'
 import { useEffect } from 'react'
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
-
+  // Track route changes for GA
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
-  }, [router.events]);
-
-
-
-  useEffect(() => {
-    // Init Tawk.to
-    const tawk = new TawkTo(
-      process.env.NEXT_PUBLIC_TAWK_PROPERTY_ID,
-      process.env.NEXT_PUBLIC_TAWK_ID
-    )
-
-    tawk.onStatusChange((status) => {
-      // console.log(status)
-    })
-
-    // Disable right-click and dev tools
-    const handleContextMenu = (e) => e.preventDefault()
-    const handleKeyDown = (e) => {
-      if (
-        (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
-        e.key === 'F12'
-      ) {
-        e.preventDefault()
-      }
+      gtag.pageview(url)
     }
-
-    // document.addEventListener('contextmenu', handleContextMenu)
-    // document.addEventListener('keydown', handleKeyDown) // Uncomment to block keys
-
-    return () => {
-      document.removeEventListener('contextmenu', handleContextMenu)
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [])
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => router.events.off('routeChangeComplete', handleRouteChange)
+  }, [router.events])
 
   return (
     <Layout>
-      {/* Google Ads tracking */}
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=AW-966026451"
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-966026451');
-        `}
-      </Script>
-
-      {/* google analytics */}
+      {/* Google Analytics + Google Ads */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID}`}
       />
       <Script
-        id="google-analytics"
+        id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-      // Google Analytics
-      gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID}', {
-        page_path: window.location.pathname,
-      });
+            // Google Analytics
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
 
-      // Google Ads
-      gtag('config', 'AW-966026451');
-    `,
+            // Google Ads
+            gtag('config', 'AW-966026451');
+          `,
         }}
       />
 
+      {/* Tawk.to widget */}
+      <Script
+        id="tawkto"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/${process.env.NEXT_PUBLIC_TAWK_PROPERTY_ID}/${process.env.NEXT_PUBLIC_TAWK_ID}';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
 
+      {/* Zoho SalesIQ */}
+      <Script
+        id="zoho-salesiq"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.$zoho = window.$zoho || {};
+            $zoho.salesiq = $zoho.salesiq || { ready:function(){} };
+          `,
+        }}
+      />
+      <Script
+        src="https://salesiq.zohopublic.com/widget?wc=siq45906fdec817e7bc305045159438b409c7d9d2a0921da2ee29bafbbf82da3c75"
+        strategy="afterInteractive"
+      />
+
+      {/* Toasts */}
       <ToastContainer position="top-center" />
-      <Component {...pageProps} />
 
-      <script>window.$zoho=window.$zoho || { };$zoho.salesiq=$zoho.salesiq||{ready:function(){ }}</script><script id="zsiqscript" src="https://salesiq.zohopublic.com/widget?wc=siq45906fdec817e7bc305045159438b409c7d9d2a0921da2ee29bafbbf82da3c75" defer></script>
+      {/* App content */}
+      <Component {...pageProps} />
     </Layout>
   )
 }
