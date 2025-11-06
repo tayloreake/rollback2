bdf6addece52f2a16cc2d52b0577f59e1bb0f5ce
aceAaron Charles <charlesunlimited10@gmail.com>
2025-11-06 13:52:31 +0300

    Add job application notice to quote form to redirect job seekers

--- components/Quote/Form.jsx
+++ components/Quote/Form.jsx
@@ -353,6 +353,34 @@ const QuoteForm = () => {
       
       {/* Form Container - Scrollable fields */}
       <div className='px-4 pb-4 quote-form-scroll-container' style={{ maxHeight: 'calc(90vh - 240px)', overflowY: 'auto', overflowX: 'hidden' }}>
+        {/* Job Application Notice */}
+        <div className='mb-4 p-3 bg-blue-50 border border-blue-200 rounded-lg'>
+          <div className='flex items-start space-x-2'>
+            <div className='flex-shrink-0 mt-0.5'>
+              <svg className='w-5 h-5 text-blue-600' fill='currentColor' viewBox='0 0 20 20'>
+                <path fillRule='evenodd' d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z' clipRule='evenodd' />
+              </svg>
+            </div>
+            <div className='flex-1'>
+              <p className='text-sm text-blue-800 font-medium mb-1'>
+                Looking for a job?
+              </p>
+              <p className='text-xs text-blue-700'>
+                This form is for moving service quotes only. If you're seeking employment, please{' '}
+                <a 
+                  href='/Jobs' 
+                  className='font-semibold underline hover:text-blue-900 transition-colors'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  visit our Jobs page
+                </a>
+                {' '}to apply.
+              </p>
+            </div>
+          </div>
+        </div>
+
         <form
           ref={formRef}
           onSubmit={handleSubmit}
