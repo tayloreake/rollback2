94c62a83058bf87db782396445550112110638e1
tayloreake <info@taylorea.com>
2026-05-14 15:17:30 +0300

    Integrate Zoho SalesIQ into Layout component
    Added Zoho SalesIQ scripts for integration.

--- components/Layout.js
+++ components/Layout.js
@@ -15,6 +15,22 @@ const Layout = ({ children, caseStudies = [] }) => {
       <PerformanceOptimizer />
       {children}
       <Footer caseStudies={caseStudies} />
+    {/* Zoho SalesIQ */}
+      <Script id="zoho-salesiq-init" strategy="afterInteractive">
+        {`
+          window.$zoho = window.$zoho || {};
+          window.$zoho.salesiq = window.$zoho.salesiq || {
+            widgetcode: "siq45906fdec817e7bc305045159438b409c7d9d2a0921da2ee29bafbbf82da3c75",
+            values: {},
+            ready: function() {}
+          };
+        `}
+      </Script>
+      <Script
+        id="zsiqscript"
+        src="https://salesiq.zohopublic.com/widget?wc=siq45906fdec817e7bc305045159438b409c7d9d2a0921da2ee29bafbbf82da3c75"
+        strategy="afterInteractive"
+      />
     </>
   )
 }
